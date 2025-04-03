cc6248d476c26b4bc2a38f34357d54f1d37e760b
ocean <berdyuginka@gmail.com>
2025-04-03 18:05:12 +0300

    started reactivity

--- turbine.py
+++ turbine.py
@@ -11,3 +11,13 @@ def calculate_reactivity(l, d1, alpha1, alpha2, beta1, beta2, low_type):
         beta2 (_type_): бетта2
         low_type (_type_): тип закона (существуют разные формулы, чтобы можно было поразному считать)
     """
+    if low_type == 1:
+        rho = 89
+    elif low_type == 2:
+        rho = 90
+    else:
+        rho = 1
+    return rho
+
+if __name__ == '__main__':
+    print(calculate_reactivity(1, 1, 1, 1, 1, 1, 2))
