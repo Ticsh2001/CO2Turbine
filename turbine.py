5c015a974f4e6b675e43507412d3a2f5060845c0
Victor Tishchenko <ticsh2001@yandex.ru>
2025-04-03 17:51:05 +0300

    task1

---
+++ turbine.py
@@ -0,0 +1,13 @@
+
+def calculate_reactivity(l, d1, alpha1, alpha2, beta1, beta2, low_type):
+    """Функция расчета степени реактивности ступени
+
+    Args:
+        l (_type_): высота лопатки
+        d1 (_type_): средний диаметр
+        alpha1 (_type_): альфа1
+        alpha2 (_type_): альфа2
+        beta1 (_type_): бетта1
+        beta2 (_type_): бетта2
+        low_type (_type_): тип закона (существуют разные формулы, чтобы можно было поразному считать)
+    """
